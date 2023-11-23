456b25c28edced0b73d147bd74da59d7f0fced40
harryprayiv <harryprayiv@gmail.com>
2023-11-23 18:42:13 -0500

    reading file reliably

--- src/Main.purs
+++ src/Main.purs
@@ -4,7 +4,18 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Console (log)
+import Effect.Aff (launchAff_)
+import Node.FS.Aff (readTextFile)
+import Yoga.JSON as JSON
+import Node.Encoding (Encoding(..))
+import Data.Either (hush)
+import Data.Maybe (fromMaybe)
+import Effect.Class.Console (logShow)
+
+type Player = { first_name :: String, last_name :: String }
 
 main :: Effect Unit
-main = do
-  log "🍝"
+main = launchAff_ $ do
+  fileContent <- readTextFile UTF8 "/home/bismuth/plutus/workspace/devWs/simpleDraft/src/activePlayers.json"
+  let players = fromMaybe [] $ hush $ JSON.readJSON fileContent
+  logShow (players :: Array Player)
