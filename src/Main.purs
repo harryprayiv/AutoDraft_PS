7e8d4f5663ffdcbcb18db464ea9476764c6ba7bd
harryprayiv <harryprayiv@gmail.com>
2023-11-24 11:43:15 -0500

    use PlutusExp Architecture = no  🛞 invention

--- src/Main.purs
+++ src/Main.purs
@@ -1,41 +1,10 @@
-module Main where
+module Main (main) where
 
-import Prelude
-
-import Data.Either (Either(..), hush)
-import Data.Map (Map)
-import Data.Map as Map
-import Data.Maybe (fromMaybe)
+import Prelude (($), Unit)
 import Effect (Effect)
-import Effect.Aff (launchAff_)
-import Effect.Class.Console (logShow)
-import Effect.Console (log)
-import Node.Encoding (Encoding(..))
-import Node.FS.Aff (readTextFile)
-import Yoga.JSON as JSON
+import Form (render, form)
 
-type Player =
-  { active :: Boolean
-  , batSide :: String
-  , currentTeam :: Int
-  , nameSlug :: String
-  , pitchHand :: String
-  , playerId :: Int
-  , primaryPosition :: String
-  , useLastName :: String
-  , useName :: String
-  }
-
-type Players = Map String Player
-
-type ActivePlayers =
-  { checksum :: String
-  , dataPulled :: String
-  , officialPlayers :: Players
-  }
+foreign import setHTML :: String -> Effect Unit
 
 main :: Effect Unit
-main = launchAff_ $ do
-  fileContent <- readTextFile UTF8 "./appData/rosters/activePlayers.json"
-  let parsedData = fromMaybe { checksum: "", dataPulled: "", officialPlayers: Map.empty } $ hush $ JSON.readJSON fileContent
-  logShow (parsedData :: ActivePlayers)
+main = setHTML $ render form
