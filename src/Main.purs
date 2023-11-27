388b120041608b08c5bf75389721a4741b7d1363
harryprayiv <harryprayiv@gmail.com>
2023-11-27 15:15:24 -0500

    crazed

--- src/Main.purs
+++ src/Main.purs
@@ -5,6 +5,7 @@ import Prelude
 import Data.Traversable (traverse)
 import Effect (Effect)
 import Effect.Aff (Aff)
+import Effect.Aff.Compat (EffectFnAff)
 import Effect.Class.Console (log)
 import Web.HTML (window)
 import Web.DOM.Document (createElement)
@@ -19,20 +20,24 @@ import Web.Event.Event (Event, EventType(..), preventDefault)
 import Web.Event.EventTarget (eventListener, addEventListener)
 import Web.HTML.HTMLInputElement (fromElement, value)
 import Web.DOM.ParentNode (QuerySelector(..), querySelector)
+import Affjax (AffjaxDriver, request, defaultRequest, printError, Response)
 import Affjax (AffjaxDriver, get, request, defaultRequest)
 import Affjax.Web as AW
 import Affjax.ResponseHeader
 import Affjax.ResponseFormat (json)
 import Data.Argonaut.Decode (decodeJson)
 import Data.Argonaut.Decode.Error (JsonDecodeError, printJsonDecodeError)
+import Data.Argonaut.Decode.Class (class DecodeJson, (.=), (.?))
 import Data.Argonaut.Parser (jsonParser)
 import Data.Map as Map
 import Form (render, form)
 import Data.Maybe (Maybe(..))
 import Data.Either (Either(..))
 import Data.Foldable (traverse_)
+import Foreign
 
 foreign import _ajax :: 
+  forall a.
   AffjaxDriver -> 
   String -> 
   String -> 
@@ -54,6 +59,32 @@ type AjaxRequest a =
 
 foreign import setHTML :: String -> Effect Unit
 
+type ActivePlayers = { officialPlayers :: Map String Player }
+
+instance decodeJsonActivePlayers :: DecodeJson ActivePlayers where
+  decodeJson json = do
+    obj <- toObject json
+    players <- obj .? "officialPlayers"
+    pure { officialPlayers: players }
+
+instance decodeJsonPlayer :: DecodeJson Player where
+  decodeJson json = do
+    obj <- toObject json
+    active <- obj .? "active"
+    batSide <- obj .? "batSide"
+    currentTeam <- obj .? "currentTeam"
+    nameSlug <- obj .? "nameSlug"
+    pitchHand <- obj .? "pitchHand"
+    primaryPosition <- obj .? "primaryPosition"
+    useLastName <- obj .? "useLastName"
+    useName <- obj .? "useName"
+    pure $ Player active batSide currentTeam nameSlug pitchHand primaryPosition useLastName useName
+
+instance decodeJsonPlayers :: DecodeJson (Map String Player) where
+  decodeJson json = do
+    obj <- toObject json
+    traverse decodeJson (toMap obj)
+
 type Player =
   { active :: Boolean
   , batSide :: String
@@ -67,8 +98,6 @@ type Player =
   }
 
 type Players = Map.Map String Player
-
-type ActivePlayers = { officialPlayers :: Players }
 
 main :: Effect Unit
 main = do
@@ -130,16 +159,19 @@ getInputValue maybeEl = case maybeEl of
 
 loadAndFilterPlayers :: String -> Aff Players
 loadAndFilterPlayers position = do
-  let req = defaultRequest { url = "./appData/rosters/activePlayers.json", responseType = json }
+  let req = defaultRequest { url = "./appData/rosters/activePlayers.json", responseFormat = json }
   response <- request AW.driver req
   case response of
     Left error -> do
-      log $ "Error loading JSON: " <> show error
+      log $ "Error loading JSON: " <> printError error
       pure Map.empty
-    Right res -> case decodeJson res.body of
-      Right activePlayers -> 
-        pure $ Map.filter (\player -> player.primaryPosition == position) (activePlayers.officialPlayers)
-      Left decodeError -> do
-        log $ "Error parsing JSON: " <> printJsonDecodeError decodeError
-        pure Map.empty
+    Right res -> 
+      case decodeJson res.body of
+        Right activePlayers -> 
+          pure $ Map.filter (\player -> player.primaryPosition == position) (activePlayers.officialPlayers)
+        Left decodeError -> do
+          log $ "Error parsing JSON: " <> printJsonDecodeError decodeError
+          pure Map.empty
+
+
 
