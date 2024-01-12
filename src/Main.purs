70da5f4f0ec75488f77358ed6694d0561bae85b2
harryprayiv <harryprayiv@gmail.com>
2024-01-12 17:06:24 -0500

    this is a mess

--- src/Main.purs
+++ src/Main.purs
@@ -6,52 +6,35 @@ import CSS.Selector
 import CSS.Stylesheet
 import CSS.Text.Transform
 import Prelude
+import Prelude
 import Web.HTML.Common
--- import CSS (Rule(..), color, fromString, lighter, zIndex)
--- import CSS as CSS
--- import CSS.Background (backgroundColor, background)
--- import CSS.Border (border, borderTop, solid)
--- import CSS.Box (boxShadow)
--- import CSS.Color (rgba, white)
--- import CSS.Common (none)
--- import CSS.Cursor (cursor)
--- import CSS.Display (absolute, opacity, position)
--- import CSS.Font (GenericFontFamily(..), bold, fontFamily, fontSize, fontWeight, sansSerif, serif)
--- import CSS.Geometry (top, left, width, height, padding, margin)
--- import CSS.Geometry (top, left, width, height, padding, margin) as Geo
--- import CSS.Gradient (radialGradient, circle, closestSide)
--- import CSS.Property (Key(..), Literal(..), Prefixed(..), Value, value)
--- import CSS.Selector (element) as CSel
--- import CSS.Size (px)
--- import CSS.Size (px, pct, em)
--- import CSS.Stylesheet (key, rule, (?), CSS)
--- import CSS.Text (letterSpacing)
--- import CSS.Text.Transform (uppercase, capitalize)
--- import CSS.TextAlign (center, textAlign)
+
 import Data.Array (deleteAt, fold, insertAt, mapWithIndex, splitAt, (!!))
+import Data.Array (deleteAt, insertAt, (!!))
 import Data.Function (identity)
 import Data.Int (toNumber)
-import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Maybe (Maybe(..), fromMaybe, maybe)
+import Data.Maybe (fromMaybe)
 import Data.NonEmpty (NonEmpty, (:|))
 import Data.String (Pattern(..))
 import Data.Tuple (Tuple(..))
+import Effect (Effect)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Aff.Class (class MonadAff)
 import Effect.Class.Console (log)
 import Effect.Unsafe (unsafePerformEffect)
-
 import Halogen (ClassName(..), ElemName(..))
+import Halogen as H
 import Halogen as H
 import Halogen.Aff as HA
 import Halogen.Aff.Driver as H
 import Halogen.HTML (HTML)
-import Halogen.HTML (HTML)
+import Halogen.HTML (HTML, IProp)
 import Halogen.HTML as HH
-import Halogen.HTML.CSS (style)
-import Halogen.HTML.CSS (stylesheet)
 import Halogen.HTML.Elements (div, element)
 import Halogen.HTML.Elements as HEL
+import Halogen.HTML.Events (handler)
 import Halogen.HTML.Events as HEV
 import Halogen.HTML.Properties as HP
 import Halogen.Store.Connect (Connected, connect)
@@ -61,11 +44,11 @@ import Halogen.VDom.Driver (runUI)
 import Halogen.VDom.Types (ElemName(..))
 import Web.DOM (Element)
 import Web.Event.Event (Event, EventType(..))
+import Web.HTML.Event.DragEvent.EventTypes as DE
+import Web.PointerEvent.PointerEvent (PointerEvent, toPointerEvent, toMouseEvent)
 import Web.UIEvent.MouseEvent (MouseEvent(..), clientX, clientY, toEvent)
 import Web.UIEvent.MouseEvent as MouseEvent
-import Web.UIEvent.MouseEvent.EventTypes as MouseEvent
-import Styling (renderStylesheet)
-
+import Web.UIEvent.MouseEvent.EventTypes as MET
 
 type Input = Unit
 
@@ -81,6 +64,7 @@ type DragState =
   , originalY :: Int
   , currentX :: Int
   , currentY :: Int
+  , isDragging :: Boolean
   }
 
 type Store = 
@@ -90,9 +74,9 @@ type Store =
   }
 
 data Action
-  = StartDrag Int Int Int  -- include the starting x and y positions
-  | MoveDrag Int Int       -- include the new x and y positions
-  | DragOver Int           -- include the index of the row being dragged over
+  = StartDrag Int Int Int
+  | MoveDrag Int Int
+  | DragOver Int
   | EndDrag
 
 initialStore :: Store
@@ -108,73 +92,56 @@ initialStore =
   , dragOverIndex: Nothing
   }
 
-component
-  :: forall query output m
-   . MonadStore Action Store m
-  => H.Component query Input output m
-component = connect selectStore $ H.mkComponent
-  { initialState: deriveState
+component :: forall q o m. MonadAff m => H.Component q Unit o m
+component = H.mkComponent
+  { initialState: const initialStore
   , render
-  , eval: H.mkEval $ H.defaultEval
-      { handleAction = handleAction
-      }
+  , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
   }
 
-render :: forall m. MonadAff m => Store -> H.ComponentHTML Action () m
+render :: forall m. Store -> H.ComponentHTML Action () m
 render state =
   HH.div []
-    [ renderStylesheet
-    , HH.table
+    [ HH.table
         [ HP.class_ (HH.ClassName "draggable-table") ]
-        [ HH.thead_
-          [ HH.tr_
-            [ HH.th_ [ HH.text "Name" ]
-            , HH.th_ [ HH.text "Occupation" ]
-            ]
-          ]
-        , HH.tbody_
-            $ mapWithIndex (\index row -> renderRow index row state.dragging) state.rows
+        [ HH.thead_ [ HH.tr_ [ HH.th_ [ HH.text "Name" ], HH.th_ [ HH.text "Occupation" ]]]
+        , HH.tbody_ $ mapWithIndex (\index row -> renderRow index row state.dragState.index) state.rows
         ]
     ]
 
-renderRow :: forall m. MonadAff m => Connected Store Input -> Int -> String -> Maybe Int -> H.ComponentHTML Action () m
-renderRow connected index row dragging =
+renderRow :: forall m. Int -> String -> Maybe Int -> H.ComponentHTML Action () m
+renderRow index row dragging =
   HH.tr
-    [ HP.classes $ ClassName <$> ["draggable-table__row"] <> 
-        if Just index == connected.context.dragState.index then ["is-dragging"] 
-        else if Just index == connected.context.dragOverIndex then ["drag-over"] 
-        else []
-    , HEV.onMouseDown $ \event -> handleMouseDownEvent (toEvent event) connected.dispatch
-    , HEV.onMouseMove $ \event -> handleMouseMoveEvent event connected.dispatch
-    , HEV.onMouseUp $ const $ connected.dispatch EndDrag
-    , HEV.onDragOver $ const $ connected.dispatch $ DragOver index
+    [ HP.classes $ ClassName <$> ["draggable-table__row"] <>
+        if Just index == dragging then ["is-dragging"] else []
+    , onMouseDown $ \event -> 
+        let
+          x = MouseEvent.clientX event
+          y = MouseEvent.clientY event
+        in
+          StartDrag index x y
+    , onMouseMove $ \event ->
+        let
+          x = MouseEvent.clientX event
+          y = MouseEvent.clientY event
+        in
+          MoveDrag x y
+    , onMouseUp $ const EndDrag
+    , onDragOver $ const $ DragOver index
     , HP.draggable true
     ]
-    $ map renderCell (splitAt 15 row)
-  where
-    handleMouseDownEvent :: Event -> (Action -> Unit) -> Unit
-    handleMouseDownEvent event dispatch = 
-      case MouseEvent.fromEvent event of
-        Just mouseEvent -> dispatch $ StartDrag index (MouseEvent.clientX mouseEvent) (MouseEvent.clientY mouseEvent)
-        Nothing -> dispatch EndDrag -- Fallback in case of an event type mismatch
+    [ HH.td_ [ HH.text row ] ]
 
-    handleMouseMoveEvent :: Event -> (Action -> Unit) -> Unit
-    handleMouseMoveEvent event dispatch =
-      case MouseEvent.fromEvent event of
-        Just mouseEvent -> dispatch $ MoveDrag (MouseEvent.clientX mouseEvent) (MouseEvent.clientY mouseEvent)
-        Nothing -> dispatch EndDrag -- Fallback in case of an event type mismatch
-
-
-reduce :: Store -> Action -> Store
-reduce store action = case action of
-  StartDrag index x y -> 
-    store { dragState = { index: Just index, originalX: x, originalY: y, currentX: x, currentY: y }}
-  MoveDrag x y -> 
-    store { dragState = store.dragState { currentX: x, currentY: y }}
-  DragOver index -> 
-    store { dragOverIndex = Just index }
-  EndDrag -> 
-    store { dragState = store.dragState { index: Nothing, currentX: 0, currentY: 0 }, dragOverIndex = Nothing }
+-- reduce :: Store -> Action -> Store
+-- reduce store action = case action of
+--   StartDrag index x y -> 
+--     store { dragState = { index: (Just index), originalX: x, originalY: y, currentX: x, currentY: y }}
+--   MoveDrag x y -> 
+--     store { dragState = store.dragState { currentX: x, currentY: y }}
+--   DragOver index -> 
+--     store { dragOverIndex = Just index }
+--   EndDrag -> 
+--     store { dragState = store.dragState { index: Nothing, currentX: 0, currentY: 0 }, dragOverIndex = Nothing }
 
 
 renderCell :: forall m. MonadAff m => Int -> String -> H.ComponentHTML Action () m
@@ -183,23 +150,18 @@ renderCell index content =
     [ HP.class_ (HH.ClassName "table-cell") ]
     [ HH.text content ]  
 
-handleAction :: forall o m. MonadAff m => Action -> H.HalogenM (Connected Store Input) Action o m Unit
-handleAction action = updateStore action
-  case action of
-    StartDrag index x y -> 
-      updateStore $ StartDrag index x y
-    MoveDrag x y -> 
-      updateStore $ MoveDrag x y
-    DragOver index -> 
-      updateStore $ DragOver index
-    EndDrag -> 
-      updateStore EndDrag
+handleAction :: forall o m. MonadAff m => Action -> H.HalogenM Store Action o m Unit
+handleAction action = case action of
+  StartDrag index x y -> H.modify_ $ \s -> s { dragState = { index: Just index, originalX: x, originalY: y, currentX: x, currentY: y, isDragging: true }}
+  MoveDrag x y -> H.modify_ $ \s -> s { dragState = s.dragState { currentX = x, currentY = y }}
+  DragOver index -> H.modify_ $ \s -> s { dragOverIndex = Just index }
+  EndDrag -> H.modify_ $ \s -> s { dragState = s.dragState { index = Nothing, isDragging = false }, dragOverIndex = Nothing }
+
 
 main :: Effect Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
-  root <- runStoreT initialStore reduce component
-  void $ H.runUI root unit body
+  void $ H.runUI component unit body
 
 moveRow :: Int -> Int -> Array String -> Array String
 moveRow from to rows =
@@ -208,3 +170,22 @@ moveRow from to rows =
     rowsWithoutDragged = fromMaybe rows $ deleteAt from rows
   in
     fromMaybe rowsWithoutDragged $ insertAt to draggedRow rowsWithoutDragged
+
+-- Helper functions to create event handlers
+onMouseDown :: forall r. (MouseEvent -> Action) -> IProp (onMouseDown :: MouseEvent | r) Action
+onMouseDown f = handler MET.mousedown (\ev -> maybe identity f (toEvent ev))
+
+onMouseMove :: forall r. (MouseEvent -> Action) -> IProp (onMouseMove :: MouseEvent | r) Action
+onMouseMove f = handler MET.mousemove (\ev -> maybe identity f (toEvent ev))
+
+onMouseUp :: forall r. (MouseEvent -> Action) -> IProp (onMouseUp :: MouseEvent | r) Action
+onMouseUp f = handler MET.mouseup $ \ev -> 
+  case toMouseEvent ev of
+    Just me -> f me
+    Nothing -> identity unit
+
+
+
+
+onDragOver :: forall r. (MouseEvent -> Action) -> IProp (onDragOver :: MouseEvent | r) Action
+onDragOver f = handler DE.dragover (\ev -> maybe identity f (toEvent ev))
--- src/Styling.purs
+++ src/Styling.purs
@@ -1,166 +1,166 @@
 module Styling
-  ( bodySelector
-  , commonStyles
-  , htmlSelector
-  , myStylesheet
-  , renderStylesheet
-  , textIndentStyle
-  , userSelectStyle
-  )
+  -- ( bodySelector
+  -- , commonStyles
+  -- , htmlSelector
+  -- , myStylesheet
+  -- , renderStylesheet
+  -- , textIndentStyle
+  -- , userSelectStyle
+  -- )
   where
 
-import Prelude
-import Web.HTML.Common
+-- import Prelude
+-- import Web.HTML.Common
 
-import CSS (Rule(..), star, color, fromString, lighter, zIndex)
-import CSS.Background (backgroundColor, background)
-import CSS.Border (border, borderTop, solid)
-import CSS.Box (boxShadow)
-import CSS.Color (rgba, white)
-import CSS.Color (rgba, white) as Color
-import CSS.Common (none)
-import CSS.Cursor (cursor)
-import CSS.Display (absolute, opacity, position)
-import CSS.Font (GenericFontFamily(..), bold, fontFamily, fontSize, fontWeight, sansSerif, serif)
-import CSS.Geometry (top, left, width, height, padding, margin)
-import CSS.Geometry as CSS
-import CSS.Gradient (radialGradient, circle, closestSide)
-import CSS.Property (Key(..), Literal(..), Prefixed(..), Value, value)
-import CSS.Selector (Selector, byClass, deep, element) as CSel
-import CSS.Selector (deep)
-import CSS.Size (px, pct, em)
-import CSS.Stylesheet (key, rule, (?), CSS)
-import CSS.Text (letterSpacing)
-import CSS.Text.Transform (capitalize, textTransform, uppercase)
-import CSS.TextAlign (center, textAlign)
-import Data.Int (toNumber)
-import Data.NonEmpty (NonEmpty, (:|))
-import Data.Tuple (Tuple(..))
-import Data.Tuple (Tuple)
-import Halogen.HTML (HTML)
-import Halogen.HTML.CSS (style)
-import Halogen.HTML.CSS (stylesheet)
-import Halogen.HTML.Elements (div, element) as HEL
-import Halogen.HTML.Events as HEV
-import Halogen.HTML.Properties as HP
-import Halogen.Store.Connect (Connected, connect)
-import Halogen.Store.Monad (class MonadStore, StoreT, runStoreT, updateStore)
+-- import CSS (Rule(..), star, color, fromString, lighter, zIndex)
+-- import CSS.Background (backgroundColor, background)
+-- import CSS.Border (border, borderTop, solid)
+-- import CSS.Box (boxShadow)
+-- import CSS.Color (rgba, white)
+-- import CSS.Color (rgba, white) as Color
+-- import CSS.Common (none)
+-- import CSS.Cursor (cursor)
+-- import CSS.Display (absolute, opacity, position)
+-- import CSS.Font (GenericFontFamily(..), bold, fontFamily, fontSize, fontWeight, sansSerif, serif)
+-- import CSS.Geometry (top, left, width, height, padding, margin)
+-- import CSS.Geometry as CSS
+-- import CSS.Gradient (radialGradient, circle, closestSide)
+-- import CSS.Property (Key(..), Literal(..), Prefixed(..), Value, value)
+-- import CSS.Selector (Selector, byClass, deep, element) as CSel
+-- import CSS.Selector (deep)
+-- import CSS.Size (px, pct, em)
+-- import CSS.Stylesheet (key, rule, (?), CSS)
+-- import CSS.Text (letterSpacing)
+-- import CSS.Text.Transform (capitalize, textTransform, uppercase)
+-- import CSS.TextAlign (center, textAlign)
+-- import Data.Int (toNumber)
+-- import Data.NonEmpty (NonEmpty, (:|))
+-- import Data.Tuple (Tuple(..))
+-- import Data.Tuple (Tuple)
+-- import Halogen.HTML (HTML)
+-- import Halogen.HTML.CSS (style)
+-- import Halogen.HTML.CSS (stylesheet)
+-- import Halogen.HTML.Elements (div, element) as HEL
+-- import Halogen.HTML.Events as HEV
+-- import Halogen.HTML.Properties as HP
+-- import Halogen.Store.Connect (Connected, connect)
+-- import Halogen.Store.Monad (class MonadStore, StoreT, runStoreT, updateStore)
 
-commonStyles :: CSS
-commonStyles = do
-  deep htmlSelector bodySelector ? do
-    padding (px 0.0)
-    margin (px 0.0) 
-    CSS.width (pct 100.0)  -- Directly set the width
-    CSS.height (pct 100.0) -- Directly set the height
-    -- key (Key (fromString "width")) (pct 100.0)  -- Set the width property
-    -- key (Key (fromString "height")) (pct 100.0) -- Set the height property
-    -- height (pct 100.0)
-    backgroundColor (rgba 203 56 233 1.0)
-    background (radialGradient (circle closestSide) (circle closestSide) [ (Tuple (rgba 203 56 233 1.0) (pct 0.0)), (Tuple (rgba 132 47 168 1.0) (pct 100.0)) ])
+-- commonStyles :: CSS
+-- commonStyles = do
+--   deep htmlSelector bodySelector ? do
+--     padding (px 0.0)
+--     margin (px 0.0) 
+--     CSS.width (pct 100.0)  -- Directly set the width
+--     CSS.height (pct 100.0) -- Directly set the height
+--     -- key (Key (fromString "width")) (pct 100.0)  -- Set the width property
+--     -- key (Key (fromString "height")) (pct 100.0) -- Set the height property
+--     -- height (pct 100.0)
+--     backgroundColor (rgba 203 56 233 1.0)
+--     background (radialGradient (circle closestSide) (circle closestSide) [ (Tuple (rgba 203 56 233 1.0) (pct 0.0)), (Tuple (rgba 132 47 168 1.0) (pct 100.0)) ])
 
-userSelectStyle :: String -> CSS
-userSelectStyle val = do
-  star ? do
-    key (Key (fromString "user-select")) val
+-- userSelectStyle :: String -> CSS
+-- userSelectStyle val = do
+--   star ? do
+--     key (Key (fromString "user-select")) val
 
-textIndentStyle :: Int -> CSS
-textIndentStyle val = do
-  star ? do
-    key (Key (fromString "text-indent")) (px (toNumber val))
+-- textIndentStyle :: Int -> CSS
+-- textIndentStyle val = do
+--   star ? do
+--     key (Key (fromString "text-indent")) (px (toNumber val))
 
--- -- Render the stylesheet in a Halogen component
-renderStylesheet :: forall p i. HTML p i
-renderStylesheet = div [] [ myStylesheet ]
+-- -- -- Render the stylesheet in a Halogen component
+-- renderStylesheet :: forall p i. HTML p i
+-- renderStylesheet = div [] [ myStylesheet ]
 
-htmlSelector :: CSel.Selector
-htmlSelector = CSel.element "html"
+-- htmlSelector :: CSel.Selector
+-- htmlSelector = CSel.element "html"
 
-bodySelector :: CSel.Selector
-bodySelector = CSel.element "body"
+-- bodySelector :: CSel.Selector
+-- bodySelector = CSel.element "body"
 
-myStylesheet :: forall p i. HTML p i
-myStylesheet = stylesheet $ do
-  -- Apply styles to all elements
-  star ? do
-    fontFamily [] (serif :| [sansSerif])
+-- myStylesheet :: forall p i. HTML p i
+-- myStylesheet = stylesheet $ do
+--   -- Apply styles to all elements
+--   star ? do
+--     fontFamily [] (serif :| [sansSerif])
 
-  -- Apply styles to html and body using deep selector composition
-  CSel.deep htmlSelector bodySelector ? do
-    padding (px 0.0)
-    margin (px 0.0)
-    width (pct 100.0)
-    height (pct 100.0)
-    backgroundColor (rgba 203 56 233 1.0)
-    background (radialGradient (circle closestSide) [ ((rgba 203 56 233 1.0) pct 0.0), ((rgba 132 47 168 1.0) pct 100) ])
+--   -- Apply styles to html and body using deep selector composition
+--   CSel.deep htmlSelector bodySelector ? do
+--     padding (px 0.0)
+--     margin (px 0.0)
+--     width (pct 100.0)
+--     height (pct 100.0)
+--     backgroundColor (rgba 203 56 233 1.0)
+--     background (radialGradient (circle closestSide) [ ((rgba 203 56 233 1.0) pct 0.0), ((rgba 132 47 168 1.0) pct 100) ])
     
-  -- Apply styles to paragraphs
-  CSel.element "p" ? do
-    fontSize (em 0.75)
-    fontWeight bold
-    position absolute
-    top (pct 15.0)
-    key (Key (fromString "width")) (pct 100.0)  -- Set the width property
-    letterSpacing (px 5.0)
-    textTransform uppercase
-    textAlign center
-    color white
+--   -- Apply styles to paragraphs
+--   CSel.element "p" ? do
+--     fontSize (em 0.75)
+--     fontWeight bold
+--     position absolute
+--     top (pct 15.0)
+--     key (Key (fromString "width")) (pct 100.0)  -- Set the width property
+--     letterSpacing (px 5.0)
+--     textTransform uppercase
+--     textAlign center
+--     color white
 
-  -- Apply styles to .drag-over class
-  CSel.byClass "drag-over" ? do
-    backgroundColor (rgba 220 220 220 1.0)
+--   -- Apply styles to .drag-over class
+--   CSel.byClass "drag-over" ? do
+--     backgroundColor (rgba 220 220 220 1.0)
 
-  -- Apply styles to .draggable-table class
-  CSel.byClass "draggable-table" ? do
-    position absolute
-    top (pct 25)
-    left (pct 20)
-    width (pct 60)
-    height (pct 50)
-    boxShadow "0px 0px 10px 8px rgba(0, 0, 0, 0.1)"
-    backgroundColor white
+--   -- Apply styles to .draggable-table class
+--   CSel.byClass "draggable-table" ? do
+--     position absolute
+--     top (pct 25)
+--     left (pct 20)
+--     width (pct 60)
+--     height (pct 50)
+--     boxShadow "0px 0px 10px 8px rgba(0, 0, 0, 0.1)"
+--     backgroundColor white
 
-    -- Nested styles for .draggable-table__drag class
-    CSel.byClass "draggable-table__drag" ? do
-      fontSize (em 0.95)
-      fontWeight lighter
-      textTransform capitalize
-      position absolute
-      width (pct 100)
-      border (px 1) solid (rgba 241 241 241 1)
-      zIndex 10
-      cursor "grabbing"
-      boxShadow "2px 2px 3px 0px rgba(0, 0, 0, 0.05)"
-      opacity 1
+--     -- Nested styles for .draggable-table__drag class
+--     CSel.byClass "draggable-table__drag" ? do
+--       fontSize (em 0.95)
+--       fontWeight lighter
+--       textTransform capitalize
+--       position absolute
+--       width (pct 100)
+--       border (px 1) solid (rgba 241 241 241 1)
+--       zIndex 10
+--       cursor "grabbing"
+--       boxShadow "2px 2px 3px 0px rgba(0, 0, 0, 0.05)"
+--       opacity 1
 
-  -- Apply styles to table heads
-  CSel.deep (CSel.byClass "draggable-table") (CSel.element "thead") ? do
-    CSel.element "th" ? do
-      height (px 25)
-      fontWeight bold
-      textTransform capitalize
-      padding (px 10)
+--   -- Apply styles to table heads
+--   CSel.deep (CSel.byClass "draggable-table") (CSel.element "thead") ? do
+--     CSel.element "th" ? do
+--       height (px 25)
+--       fontWeight bold
+--       textTransform capitalize
+--       padding (px 10)
 
-  -- Apply styles to table body
-  CSel.deep (CSel.byClass "draggable-table") (CSel.element "tbody") ? do
-    CSel.element "tr" ? do
-      cursor "grabbing"
+--   -- Apply styles to table body
+--   CSel.deep (CSel.byClass "draggable-table") (CSel.element "tbody") ? do
+--     CSel.element "tr" ? do
+--       cursor "grabbing"
 
-      CSel.element "td" ? do
-        fontSize (em 0.95)
-        fontWeight lighter
-        textTransform capitalize
-        padding (px 10)
-        borderTop (px 1) solid (rgba 245 245 245 1)
+--       CSel.element "td" ? do
+--         fontSize (em 0.95)
+--         fontWeight lighter
+--         textTransform capitalize
+--         padding (px 10)
+--         borderTop (px 1) solid (rgba 245 245 245 1)
 
-    CSel.element "tr:nth-child(even)" ? do
-      backgroundColor (rgba 247 247 247 1)
+--     CSel.element "tr:nth-child(even)" ? do
+--       backgroundColor (rgba 247 247 247 1)
 
-    CSel.element "tr:nth-child(odd)" ? do
-      backgroundColor white
+--     CSel.element "tr:nth-child(odd)" ? do
+--       backgroundColor white
 
-    CSel.element "tr.is-dragging" ? do
-      backgroundColor (rgba 241 196 15 1)
-      CSel.element "td" ? do
-        color (rgba 255 230 131 1)
+--     CSel.element "tr.is-dragging" ? do
+--       backgroundColor (rgba 241 196 15 1)
+--       CSel.element "td" ? do
+--         color (rgba 255 230 131 1)
 
