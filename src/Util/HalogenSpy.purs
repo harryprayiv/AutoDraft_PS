fd4bfd3a3334584c82acd3a7ef9eef5159af1ed6
harryprayiv <harryprayiv@gmail.com>
2024-03-05 19:49:33 -0500

    commented out to silence warnings

--- src/Util/HalogenSpy.purs
+++ src/Util/HalogenSpy.purs
@@ -1,47 +1,26 @@
 module Util.HalogenSpy where
 
+-- import Prelude
+-- import DraftComponent
 
+-- import Data.Array
+-- import Data.Foldable (foldMap)
+-- import Data.Map as Map
+-- import Data.Maybe (Maybe(..), fromMaybe)
+-- import Data.Show (show)
+-- import Effect.Console as CONSOLE
+-- import Types.Player (PlayersMap(..), Player)
+-- import Data.Tuple
 
+-- showPlayer :: String -> Player -> String
+-- showPlayer key player = "{ Key: " <> key <> ", Player: " <> show player <> " }"
 
 -- showPlayersMap :: PlayersMap -> String
 -- showPlayersMap (PlayersMap playersMap) =
---   let
---     showPlayer :: String -> Player -> String
---     showPlayer key player = "{ Key: " <> key <> ", Player: " <> show player <> " }"
+--   "[" <> (foldMap (uncurry showPlayer) $ Map.toUnfoldable playersMap) <> "]"
 
---     playersList :: List (Tuple String Player)
---     playersList = Map.toUnfoldable playersMap
 
---     maybeNonEmptyPlayersList :: Maybe (NonEmptyList (Tuple String Player))
---     maybeNonEmptyPlayersList = toNonEmptyArray playersList
-
---     showNonEmptyPlayers :: Maybe (NonEmptyList (Tuple String Player)) -> String
---     showNonEmptyPlayers Nothing = "Empty"
---     showNonEmptyPlayers (Just nel) = intercalate ", " (map (uncurry showPlayersMap) (toListNEL nel))
---   in "[" <> showNonEmptyPlayers maybeNonEmptyPlayersList <> "]"
-
--- toListNEL :: forall a. NonEmptyList a -> List a
--- toListNEL nel = toList nel
-
--- showNonEmptyPlayers :: Maybe (NonEmptyList (Tuple String Player)) -> String
--- showNonEmptyPlayers Nothing = "Empty"
--- showNonEmptyPlayers (Just nel) = intercalate ", " (map (uncurry showPlayersMap) (toListNEL nel))
-
--- toNonEmptyArray :: forall a. Array a -> Maybe (NonEmpty Array a)
--- toNonEmptyArray arr = case Array.uncons arr of
---   Nothing -> Nothing
---   Just { head: x, tail: xs } -> Just (x :| xs)
-
--- spyHalogenState :: DebugWarning => String -> State -> State
+-- spyHalogenState :: forall state. Show state => String -> State -> State
 -- spyHalogenState msg state = 
---   let
---     stateString = "{ allPlayers: " <> showPlayersMap state.allPlayers
---                    <> ", players: " <> showPlayersMap state.players
---                    <> ", filterInputs: " <> show state.filterInputs
---                    <> ", currentSort: " <> show state.currentSort
---                    <> ", loading: " <> show state.loading
---                    <> ", error: " <> show state.error
---                    <> ", sortChangeFlag: " <> show state.sortChangeFlag
---                    <> " }"
---   in
---     spyWith msg (\_ -> stateString) state                      
+--   CONSOLE.log (msg <> ": " <> show state)
+--   state              
